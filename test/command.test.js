b6fa5b01c507566f6d72f12385283c5468d48d4f
Ido Lev <idolev@Ido-Lev.local>
2018-09-10 06:17:50 +0300

    tests

--- test/command.test.js
+++ test/command.test.js
@@ -2,6 +2,17 @@ const command = require('../lib/command');
 const { expect } = require('chai');
 
 describe('Get command', ()=> {
+    it('Should throw error', done => {
+        function throwsWithNoArgs() {
+            try {
+                command();
+            } catch (error) {
+                new ErrorThrowingObject(error);
+            }
+        }
+        expect(throwsWithNoArgs).to.throw
+        done()
+    });
     it('Should return string', () => {
         expect(command('user', 'my_app', 'my_safe')).to.be.a('string'); 
     });
--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,17 @@ const index = require('../index');
 const { expect } = require('chai');
 
 describe('Index', ()=> {
+    it('Should throw error', done => {
+        async function throwsWithNoArgs() {
+            try {
+                const pwd = await index();
+            } catch (error) {
+                new ErrorThrowingObject(error);
+            }
+        }
+        expect(throwsWithNoArgs).to.throw
+        done()
+    });
     it('Should return password', async () => {
         const pwd = await index('user', 'my_appID', 'my_safe');
         expect(pwd).to.be.a('string'); 
