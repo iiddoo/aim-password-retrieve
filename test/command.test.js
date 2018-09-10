220615a6834bc704b7cecad71aabfb9fa737a6ed
Ido Lev <idolev@Ido-Lev.local>
2018-09-10 06:24:50 +0300

    tests

--- test/command.test.js
+++ test/command.test.js
@@ -2,10 +2,6 @@ const command = require('../lib/command');
 const { expect } = require('chai');
 
 describe('Get command', ()=> {
-    it('Should throw error', done => {
-        expect(command()).to.throw
-        done()
-    });
     it('Should return string', () => {
         expect(command('user', 'my_app', 'my_safe')).to.be.a('string'); 
     });
